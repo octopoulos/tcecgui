200c6bc0048f254e47253c7bd17cf26907a78e3e
octopoulo <polluxyz@gmail.com>
2020-04-17 02:43:01 +0200

    tcecextra 4 spaces indent

--- dist/js/tcecxtra.js
+++ dist/js/tcecxtra.js
@@ -22,8 +22,8 @@ function fixOrder()
 
     let sorted = arr.slice().sort((a, b) => (b - a)),
         ranks = arr.slice().map(function(v) {return sorted.indexOf(v) + 1;});
-   LS("Ranks is :" + ranks);
-   count = 0;
+    LS("Ranks is :" + ranks);
+    count = 0;
     let tiePoints = 0;
 
     Keys(engines).forEach(key => {
@@ -32,85 +32,74 @@ function fixOrder()
         count ++;
     });
 
-   count = 0;
+    count = 0;
     Keys(engines).forEach(key => {
         let engine = engines[key];
-      engine.Neustadtl = 0;
-      tiePoints = 0;
+        engine.Neustadtl = 0;
+        tiePoints = 0;
 
         Keys(engines).forEach(ikey => {
             let iengine = engines[ikey];
-         if (ikey != key)
-         {
-            let sbCount = 0;
-            for (let i = 0; i < engine.Results[ikey].Text.length ; i++)
+            if (ikey != key)
             {
-               if (engine.Results[ikey].Text[i] == '=')
-               {
-                  sbCount += 0.5;
-               }
-               else if (engine.Results[ikey].Text[i] == '0')
-               {
-               }
-               else
-               {
-                  sbCount ++;
-               }
-            }
-            if (!engineDisqualified(key))
-            {
-               engine.Neustadtl = engine.Neustadtl + sbCount * iengine.Score;
-            }
-            else
-            {
-               engine.Neustadtl = 0;
-            }
+                let sbCount = 0,
+                    text = engine.Results[ikey].Text;
+                for (let i = 0; i < text.length ; i++)
+                {
+                    if (text[i] == '=')
+                        sbCount += 0.5;
+                    else if (text[i] == '1')
+                        sbCount ++;
+                }
+                if (!engineDisqualified(key))
+                    engine.Neustadtl = engine.Neustadtl + sbCount * iengine.Score;
+                else
+                    engine.Neustadtl = 0;
 
-            LS("iengine.Rank:" + iengine.Rank + ikey + ",engine.Rank:" + engine.Rank + key);
-            if (parseInt(iengine.Rank) && parseInt(engine.Rank) == parseInt(iengine.Rank))
-            {
-               if (engine.Strikes)
-               {
-                  tiePoints -= engine.Strikes;
-               }
-               else
-               {
-                  LS("engine.Strikes: " + engine.Results[ikey].Text);
-                  if (sbCount > engine.Results[ikey].Text.length/2)
-                  {
-                     LS("key won:" + key);
-                     tiePoints += 1/100;
-                  }
-                  else if (sbCount < engine.Results[ikey].Text.length/2)
-                  {
-                     LS("key lost:" + key);
-                     tiePoints += 0/100;
-                  }
-                  else
-                  {
-                     LS("key drew:" + key);
-                     tiePoints += 0.5/100;
-                  }
-               }
+                LS("iengine.Rank:" + iengine.Rank + ikey + ",engine.Rank:" + engine.Rank + key);
+                if (parseInt(iengine.Rank) && parseInt(engine.Rank) == parseInt(iengine.Rank))
+                {
+                    if (engine.Strikes)
+                        tiePoints -= engine.Strikes;
+                    else
+                    {
+                        LS("engine.Strikes: " + engine.Results[ikey].Text);
+                        if (sbCount > engine.Results[ikey].Text.length/2)
+                        {
+                            LS("key won:" + key);
+                            tiePoints += 1/100;
+                        }
+                        else if (sbCount < engine.Results[ikey].Text.length/2)
+                        {
+                            LS("key lost:" + key);
+                            tiePoints += 0/100;
+                        }
+                        else
+                        {
+                            LS("key drew:" + key);
+                            tiePoints += 0.5/100;
+                        }
+                    }
+                }
             }
-         }
-      });
-      tiePoints += (engine.WinsAsBlack + engine.WinsAsWhite)/(100 * 100);
-      LS("tiePoints is :" + tiePoints + ", count is :" + arr[count] + " , name is :" + key + ", score:" + engine.Score);
-      //tiePoints += engine.WinAsBlack/(100 * 100 * 100);
-      tiePoints += engine.Neustadtl/(100 * 100 * 1000);
-      tiePoints += engine.Rating/(100 * 100 * 1000 * 1000);
-      tiePoints += count/(100 * 100 * 1000 * 1000 * 1000);
-      arr[count] = parseFloat(parseFloat(engine.Score) + parseFloat(tiePoints/10));
-      count ++;
-   });
+        });
 
-   sorted = arr.slice().sort((b, a) => (a - b));
-   ranks = arr.slice().map(function(v) {return sorted.indexOf(v) + 1;});
-   count = 0;
-   LS("rank kenght us :" + ranks.length);
-   LS("Ranks is :" + ranks);
-   //crosstableData.Order = ranks;
+        tiePoints += (engine.WinsAsBlack + engine.WinsAsWhite)/(100 * 100);
+        LS("tiePoints is :" + tiePoints + ", count is :" + arr[count] + " , name is :" + key + ", score:" + engine.Score);
+        //tiePoints += engine.WinAsBlack/(100 * 100 * 100);
+        tiePoints += engine.Neustadtl/(100 * 100 * 1000);
+        tiePoints += engine.Rating/(100 * 100 * 1000 * 1000);
+        tiePoints += count/(100 * 100 * 1000 * 1000 * 1000);
+        arr[count] = parseFloat(parseFloat(engine.Score) + parseFloat(tiePoints/10));
+        count ++;
+    });
+
+    sorted = arr.slice().sort((b, a) => (a - b));
+    ranks = arr.slice().map(function(v) {return sorted.indexOf(v) + 1;});
+    count = 0;
+    LS("rank kenght us :" + ranks.length);
+    LS("Ranks is :" + ranks);
+    //crosstableData.Order = ranks;
 
     Keys(engines).forEach(key => {
         let engine = engines[key];
@@ -135,67 +124,62 @@ function getEngRecSched(data, engineName)
     };
 
     Keys(data).forEach(key => {
-       let engine = data[key];
-      if (typeof engine.Moves == 'undefined')
-      {
-         return false;
-      }
-      let dQ = engineDisqualified(engineName);
-      if (dQ)
-      {
-         resultData.LossAsStrike = dQ;
-      }
-      else if (getShortEngineName(engineName) == getShortEngineName(engine.Black))
-      {
-         if (!engineDisqualified(engine.White))
-         {
-            if (engine.Result == "1-0")
+        let engine = data[key];
+        if (typeof engine.Moves == 'undefined')
+            return false;
+
+        let dQ = engineDisqualified(engineName);
+        if (dQ)
+            resultData.LossAsStrike = dQ;
+        else if (getShortEngineName(engineName) == getShortEngineName(engine.Black))
+        {
+            if (!engineDisqualified(engine.White))
             {
-               if (!crash_re.test(engine.Termination))
-               {
-                  resultData.LossAsStrike = parseInt(resultData.LossAsStrike) + 1;
-               }
-               resultData.LossAsBlack = parseInt(resultData.LossAsBlack) + 1;
+                if (engine.Result == "1-0")
+                {
+                    if (!crash_re.test(engine.Termination))
+                        resultData.LossAsStrike = parseInt(resultData.LossAsStrike) + 1;
+
+                    resultData.LossAsBlack = parseInt(resultData.LossAsBlack) + 1;
+                }
+                else if (engine.Result == "0-1")
+                {
+                    resultData.WinAsBlack = parseInt(resultData.WinAsBlack) + 1;
+                    resultData.Score = resultData.Score + 1;
+                }
+                else
+                    resultData.Score = resultData.Score + 0.5;
+
+                resultData.Games = resultData.Games + 1;
             }
-            else if (engine.Result == "0-1")
+        }
+        else if (getShortEngineName(engineName) == getShortEngineName(engine.White))
+        {
+            if (!engineDisqualified(engine.Black))
             {
-               resultData.WinAsBlack = parseInt(resultData.WinAsBlack) + 1;
-               resultData.Score = resultData.Score + 1;
+                if (engine.Result == "0-1")
+                {
+                    if (!crash_re.test(engine.Termination))
+                    {
+                        resultData.LossAsStrike = parseInt(resultData.LossAsStrike) + 1;
+                        engine.LossAsStrike = engine.LossAsStrike + 1;
+                    }
+                    resultData.LossAsWhite = parseInt(resultData.LossAsWhite) + 1;
+                }
+                else if (engine.Result == "1-0")
+                {
+                    resultData.WinAsWhite = parseInt(resultData.WinAsWhite) + 1;
+                    resultData.Score = resultData.Score + 1;
+                }
+                else
+                    resultData.Score = resultData.Score + 0.5;
+
+                resultData.Games = resultData.Games + 1;
             }
-            else
-            {
-               resultData.Score = resultData.Score + 0.5;
-            }
-            resultData.Games = resultData.Games + 1;
-         }
-      }
-      else if (getShortEngineName(engineName) == getShortEngineName(engine.White))
-      {
-         if (!engineDisqualified(engine.Black))
-         {
-            if (engine.Result == "0-1")
-            {
-               if (!crash_re.test(engine.Termination))
-               {
-                  resultData.LossAsStrike = parseInt(resultData.LossAsStrike) + 1;
-                  engine.LossAsStrike = engine.LossAsStrike + 1;
-               }
-               resultData.LossAsWhite = parseInt(resultData.LossAsWhite) + 1;
-            }
-            else if (engine.Result == "1-0")
-            {
-               resultData.WinAsWhite = parseInt(resultData.WinAsWhite) + 1;
-               resultData.Score = resultData.Score + 1;
-            }
-            else
-            {
-               resultData.Score = resultData.Score + 0.5;
-            }
-            resultData.Games = resultData.Games + 1;
-         }
-      }
-   });
-   return resultData;
+        }
+    });
+
+    return resultData;
 }
 
 function findEloDiffOld(whiteEngine, blackEngine, whiteEngName, blackEngName, score)
@@ -210,12 +194,10 @@ function findEloDiffOld(whiteEngine, blackEngine, whiteEngName, blackEngName, sc
 
 function getRating(engine, engName)
 {
-   if (engineRatingGlobalData)
-   {
-      return engineRatingGlobalData[engName].elo;
-   }
+    if (engineRatingGlobalData)
+        return engineRatingGlobalData[engName].elo;
 
-   return engine.elo;
+    return engine.elo;
 }
 
 function findEloDiff(whiteEngine, blackEngine, whiteEngName, blackEngName, score1, score2, gameno)
@@ -228,8 +210,8 @@ function findEloDiff(whiteEngine, blackEngine, whiteEngName, blackEngName, score
         w_rating = whiteEngine.Rating + k * (score1 - e1),
         b_rating = blackEngine.Rating + k * (score2 - e2);
 
-   whiteEngine.Rating = w_rating;
-   blackEngine.Rating = b_rating;
+    whiteEngine.Rating = w_rating;
+    blackEngine.Rating = b_rating;
 }
 
 function getOverallElo()
@@ -269,47 +251,42 @@ function getOverallElo()
 
 function eliminateCrash(data)
 {
-   Keys(data).forEach(key => {
-       let engine = data[key];
-      if (typeof engine.Moves == 'undefined')
-      {
-         return false;
-      }
+    Keys(data).forEach(key => {
+        let engine = data[key];
+        if (typeof engine.Moves == 'undefined')
+            return false;
 
-      if (!crash_re.test(engine.Termination))
-      {
-         if (engine.Result == "0-1")
-         {
-            LS("Calling updateResData for " + engine.Black + "white:" + engine.White + " , engine.Result:" + engine.Result + ":term:" + engine.Termination);
-            updateResData(engine.White);
-         }
-         if (engine.Result == "1-0")
-         {
-            LS("Calling updateResData for " + engine.Black + "white:" + engine.White + " , engine.Result:" + engine.Result + ":term:" + engine.Termination);
-            updateResData(engine.Black);
-         }
-      }
-   });
+        if (!crash_re.test(engine.Termination))
+        {
+            if (engine.Result == "0-1")
+            {
+                LS("Calling updateResData for " + engine.Black + "white:" + engine.White + " , engine.Result:" + engine.Result + ":term:" + engine.Termination);
+                updateResData(engine.White);
+            }
+            if (engine.Result == "1-0")
+            {
+                LS("Calling updateResData for " + engine.Black + "white:" + engine.White + " , engine.Result:" + engine.Result + ":term:" + engine.Termination);
+                updateResData(engine.Black);
+            }
+        }
+    });
 }
 
 function updateResData(engineName)
 {
     Keys(crosstableData.Table).forEach(key => {
         let value = crosstableData.Table[key];
-      if (value.OrigStrikes == undefined || value.OrigStrikes == 'undefined')
-      {
-         value.OrigStrikes = value.Strikes;
-      }
-      if (key == engineName)
-      {
-         value.Strikes = parseInt(value.Strikes) + 1;
-         LS("value is " + engineName + ",key is :" + key);
-      }
-      if (value.Strikes > 2)
-      {
-         value.Score = 0;
-      }
-   });
+        if (value.OrigStrikes == undefined || value.OrigStrikes == 'undefined')
+            value.OrigStrikes = value.Strikes;
+
+        if (key == engineName)
+        {
+            value.Strikes = parseInt(value.Strikes) + 1;
+            LS("value is " + engineName + ",key is :" + key);
+        }
+        if (value.Strikes > 2)
+            value.Score = 0;
+    });
 }
 
 function engineDisqualified(engineName)
