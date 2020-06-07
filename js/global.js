551f12968fe85c05811ad4260149c85397d9419d
octopoulo <polluxyz@gmail.com>
2020-06-07 04:45:29 +0200

    new version

--- js/global.js
+++ js/global.js
@@ -25,7 +25,7 @@ let HOST = 'https://tcec-chess.com',
         twitch: 5 * 1000,
         users: 5 * 1000,
     },
-    VERSION = '20200606';
+    VERSION = '20200607';
 
 let players = [{}, {}, {}, {}];         // current 2 players + 2 live engines
 
