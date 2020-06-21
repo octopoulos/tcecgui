cf3ec18621f5453492f9c937823c1f414816226a
octopoulo <polluxyz@gmail.com>
2020-06-22 01:47:34 +0200

    add bracket to archive games

--- js/global.js
+++ js/global.js
@@ -24,7 +24,7 @@ let HOST_ARCHIVE,
         twitch: 5 * 1000,
         users: 5 * 1000,
     },
-    VERSION = '20200614';
+    VERSION = '20200621';
 
 let players = [{}, {}, {}, {}];         // current 2 players + 2 live engines
 
