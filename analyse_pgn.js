fb1c143cf06eadb00eed6d002aa74c96a4c7b366
octopoulo <polluxyz@gmail.com>
2020-07-05 22:07:53 +0200

    handle zip files in analyse_pgn

--- analyse_pgn.js
+++ analyse_pgn.js
@@ -1,16 +1,20 @@
 // analyse_pgn.js
 // @author octopoulo <polluxyz@gmail.com>
-// @version 2020-07-04
+// @version 2020-07-05
 /*
 globals
-console, process, require
+Buffer, console, process, require
 */
 'use strict';
 
 let fs = require('fs'),
+    unzipper = require('unzipper'),
     {Floor, FormatUnit, Keys, LS, Max, Pad, SetDefault} = require('./js/common'),
     {fix_move_format} = require('./js/global'),
     {parse_pgn} = require('./js/game');
+
+// ANALYSE STATS
+////////////////
 
 /**
  * Create spaces
@@ -23,55 +27,18 @@ function create_spaces(size) {
 
 /**
  * Get Multi PGN stats
- * @param {string} name
- * @param {function} callback
+ * - accumulate all individual speeds
+ * @param {string} data
+ * @param {Object} result
  */
-function get_multi_pgn_stats(name, callback) {
-    fs.readFile(name, 'utf8', (err, data) => {
-        if (err) {
-            callback(null);
-            return;
-        }
-
-        let result = {},
-            splits = data.split(/\r?\n\r?\n\[/);
-
-        // accumulate all individual speeds
-        for (let split of splits) {
-            let dico = get_pgn_stats(split);
-            Keys(dico).forEach(key => {
-                let entry = SetDefault(result, key, []),
-                    value = dico[key];
-                entry.push(value);
-            });
-        }
-
-        // merge
-        let max_name = 0,
-            max_speed = 0,
-            max_unit = 0;
-        Keys(result).forEach(key => {
-            let stats = result[key],
-                medians = stats.map(value => value[0]),
-                median = Floor(medians.reduce((a, b) => a + b) / medians.length + 0.5);
-
-            result[key] = median;
-            max_name = Max(max_name, key.length);
-            max_speed = Max(max_speed, `${median}`.length);
-            max_unit = Max(max_unit, FormatUnit(median).length + 6);
+function get_multi_pgn_stats(data, result) {
+    data.split(/\r?\n\r?\n\[/).forEach(split => {
+        let dico = get_pgn_stats(split);
+        Keys(dico).forEach(key => {
+            let entry = SetDefault(result, key, []),
+                value = dico[key];
+            entry.push(value);
         });
-
-        let space_name = create_spaces(max_name),
-            space_speed = create_spaces(max_speed),
-            space_unit = create_spaces(max_unit),
-            text = Keys(result).sort((a, b) => a.localeCompare(b)).map(key => {
-                return [
-                    (key + space_name).slice(0, max_name),
-                    Pad(result[key], max_speed, space_speed),
-                    Pad(FormatUnit(result[key], undefined, true) + ' (nps)', max_unit, space_unit),
-                ].join(' : ');
-            }).join('\n');
-        callback(text);
     });
 }
 
@@ -144,12 +111,139 @@ function get_pgn_stats(data) {
     return result;
 }
 
-// main
-for (let name of process.argv.slice(2)) {
-    get_multi_pgn_stats(name, result => {
-        LS('```');
-        LS(name);
-        LS(result);
-        LS('```');
+/**
+ * Merge stats
+ * @param {Object} result
+ * @param {Object} options
+ * @returns {string}
+ */
+function merge_stats(result, options) {
+    let max_name = 0,
+        max_speed = 0,
+        max_unit = 0;
+    Keys(result).forEach(key => {
+        let stats = result[key],
+            medians = stats.map(value => value[0]),
+            median = Floor(medians.reduce((a, b) => a + b) / medians.length + 0.5);
+
+        result[key] = median;
+        max_name = Max(max_name, key.length);
+        max_speed = Max(max_speed, `${median}`.length);
+        max_unit = Max(max_unit, FormatUnit(median).length + 6);
     });
+
+    let keys = Keys(result),
+        space_name = create_spaces(max_name),
+        space_speed = create_spaces(max_speed),
+        space_unit = create_spaces(max_unit);
+
+    if (options.alpha)
+        keys.sort((a, b) => a.localeCompare(b));
+    else
+        keys.sort((a, b) => result[b] - result[a]);
+
+    return keys.map(key => {
+        let value = result[key];
+        return [
+            (key + space_name).slice(0, max_name),
+            Pad(value, max_speed, space_speed),
+            Pad(FormatUnit(value, undefined, true) + ' (nps)', max_unit, space_unit),
+        ].join(' : ');
+    }).join('\n');
 }
+
+/**
+ * Open a file and process it
+ * @param {string} filename
+ * @param {Object} result
+ * @param {function} callback
+ */
+function open_file(filename, result, callback) {
+    let ext = filename.split('.').slice(-1)[0];
+    if (ext == 'zip') {
+        fs.createReadStream(filename)
+            .pipe(unzipper.Parse())
+            .on('entry', function (entry) {
+                LS(entry.path);
+                entry.buffer().then(content => {
+                    let data = content.toString();
+                    get_multi_pgn_stats(data, result);
+                });
+            })
+            .on('error', () => {
+                callback(true);
+            })
+            .on('finish', () => {
+                callback();
+            });
+    }
+    else {
+        fs.readFile(filename, 'utf8', (err, data) => {
+            if (err || !data)
+                return;
+            get_multi_pgn_stats(data, result);
+            callback();
+        });
+    }
+}
+
+// STARTUP
+//////////
+
+/**
+ * Show the results
+ * @param {Object} result
+ * @param {string[]} filenames
+ * @param {Object} options
+ */
+function done(result, filenames, options) {
+    let text = merge_stats(result, options);
+    LS('```');
+    LS(filenames.map(name => name.split(/[/\\]/).slice(-1)[0]).sort().join(', '));
+    LS(text);
+    LS('```');
+}
+
+/**
+ * Main function
+ * @argument --alpha: sort results alphabetically
+ */
+function main() {
+    // extract args
+    let args = process.argv,
+        filenames = [],
+        num_arg = args.length,
+        options = {};
+
+    for (let i = 2; i < num_arg; i ++) {
+        let arg = args[i];
+        if (arg.slice(0, 2) == '--')
+            options[arg.slice(2)] = true;
+        else
+            filenames.push(arg);
+    }
+
+    // show help?
+    if (options.help) {
+        LS([
+            `Usage: node ${args[1]} [options] [files]`,
+            '',
+            'Options:',
+            '  --alpha  sort results alphabetically',
+            '  --help   show this help',
+        ].join('\n'));
+        return;
+    }
+
+    // open files
+    let left = filenames.length,
+        result = {};
+    for (let filename of filenames)
+        open_file(filename, result, err => {
+            left --;
+            if (!left)
+                done(result, filenames, options);
+        });
+}
+
+main();
