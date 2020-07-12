5612e4898a8c72f82c6807a1e4fb29d3983d6488
octopoulo <polluxyz@gmail.com>
2020-07-12 07:10:50 +0200

    bracket medal/trophies

--- js/global.js
+++ js/global.js
@@ -38,7 +38,7 @@ let HOST_ARCHIVE,
         twitch: 5 * 1000,
         users: 5 * 1000,
     },
-    VERSION = '20200711';
+    VERSION = '20200712';
 
 let players = [{}, {}, {}, {}];         // current 2 players + 2 live engines
 
