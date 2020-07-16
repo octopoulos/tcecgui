d2929a2f9a9f9f698c18fb1590648f374ef2a115
octopoulo <polluxyz@gmail.com>
2020-07-16 15:50:29 +0200

    json hidden

--- seeds.js
+++ seeds.js
@@ -46,12 +46,15 @@ let length = engines_full.length,
     seeds = calculate_seeds(length, 1),
     teams = seeds.map(seed => [seed, engines_full[seed - 1]]),
     pairs = new Array(length / 2).fill(0).map((_, id) => [teams[id * 2], teams[id * 2 + 1]]),
-    patch = pairs.map(pair => [{name: pair[0][1], seed: pair[0][0]}, {name: pair[1][1], seed: pair[1][0]}]),
-    json = JSON.stringify(patch, null, 4);
+    patch = pairs.map(pair => [{name: pair[0][1], seed: pair[0][0]}, {name: pair[1][1], seed: pair[1][0]}]);
 
-LS(`seeds:${seeds}`);
-LS(`teams:${teams}`);
+LS(seeds);
+LS(teams);
 LS(pairs);
 LS(patch);
-LS();
-LS(json);
+
+// if need to save the JSON:
+if (0) {
+    let json = JSON.stringify(patch, null, 4);
+    LS(json);
+}
