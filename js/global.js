11d4259b1a548a678e7880a651a6a49a767329ce
octopoulo <polluxyz@gmail.com>
2021-01-06 16:25:18 +0100

    fixed play button + new moves played sequentially

--- js/global.js
+++ js/global.js
@@ -1,6 +1,6 @@
 // global.js
 // @author octopoulo <polluxyz@gmail.com>
-// @version 2021-01-05
+// @version 2021-01-06
 //
 // global variables/functions shared across multiple js files
 //
@@ -37,7 +37,7 @@ let HOST_ARCHIVE,
         twitch: 5 * 1000,
         users: 5 * 1000,
     },
-    VERSION = '20210105',
+    VERSION = '20210106',
     virtual_close_popups,
     xboards = {};
 
