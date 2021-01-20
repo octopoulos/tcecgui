dac51232d6f2a6fbf28fbbddd935f55e29c4ba0b
octopoulo <polluxyz@gmail.com>
2021-01-20 01:25:56 +0100

    no font oversize in GRID

--- js/startup.js
+++ js/startup.js
@@ -1269,7 +1269,7 @@ function resize_move_lists() {
 
             // normal size: 20 + 36 + 36 = 92
             if (grid && width > scrollbar) {
-                ratio = Min(1.1, (width - scrollbar) / grid / 92);
+                ratio = Min(1, (width - scrollbar) / grid / 92);
                 iwidth = `minmax(${36 * ratio}px, 1fr)`;
             }
 
