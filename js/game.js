141e29656e1405ff9de0508c303e93f608b2626f
octopoulo <polluxyz@gmail.com>
2021-01-25 03:48:41 +0100

    scaling: 108 => 120

--- js/game.js
+++ js/game.js
@@ -866,7 +866,7 @@ function resize_move_lists() {
 
             // normal size: 28 + 40 + 40 = 108
             if (grid && width > scrollbar)
-                ratio = Min(1, (width - scrollbar) / grid / 108);
+                ratio = Min(1, (width - scrollbar) / grid / 120);
 
             let extra = grid? `grid-template-columns: repeat(${grid}, 1fr 2fr 2fr`: '';
             Style(node, `font-size:${font * ratio}px;height:${height}px;${extra};width:${wextra}`);
